fd3be63f10b5e4886153eceac9a088ca6d41ee4d
Ubuntu <ubuntu@ip-172-31-13-96.ec2.internal>
2020-05-01 00:47:26 +0000

    typing

--- ctcdecode/__init__.py
+++ ctcdecode/__init__.py
@@ -1,16 +1,20 @@
 import torch
 from ._ext import ctc_decode
+from typing import List, NamedTuple, Optional, Tuple
+
+
+Candidate = NamedTuple('Candidate', [('value', List[int]), ('score', float)])
 
 
 class CTCBeamDecoder:
     def __init__(
         self,
-        cutoff_top_n=40,
-        cutoff_prob=1.0,
-        beam_width=100,
-        num_processes=4,
-        blank_id=0,
-        log_probs_input=False
+        cutoff_top_n: int = 40,
+        cutoff_prob: float = 1.0,
+        beam_width: int = 100,
+        num_processes: int = 4,
+        blank_id: int = 0,
+        log_probs_input: bool = False
     ):
         self.cutoff_top_n = cutoff_top_n
         self.beam_width = beam_width
@@ -19,16 +23,33 @@ class CTCBeamDecoder:
         self.log_probs_input = log_probs_input
         self.cutoff_prob = cutoff_prob
 
-    def decode(self, probs, seq_lens=None):
+    def decode(
+        self,
+        probs: torch.Tensor,
+        seq_lens: Optional[torch.Tensor] = None,
+    ) -> List[List[Candidate]]:
+
         # We expect batch x seq x label_size
         probs = probs.cpu().float()
-        batch_size, max_seq_len = probs.size(0), probs.size(1)
+        batch_size, max_seq_len = probs.shape[:2]
         if seq_lens is None:
-            seq_lens = torch.IntTensor(batch_size).fill_(max_seq_len)
+            seq_lens = torch.full((batch_size, ), max_seq_len, dtype=torch.int)
         else:
             seq_lens = seq_lens.cpu().int()
 
-        return ctc_decode.beam_decode(
-            probs, seq_lens, self.beam_width, self.num_processes, self.cutoff_prob,
-            self.cutoff_top_n, self.blank_id, self.log_probs_input
+        out: List[List[Tuple[List[int], float]]] = ctc_decode.beam_decode(
+            probs,
+            seq_lens,
+            self.beam_width,
+            self.num_processes,
+            self.cutoff_prob,
+            self.cutoff_top_n,
+            self.blank_id,
+            self.log_probs_input,
         )
+
+        # convert to named tuples
+        outt = []
+        for batch_out in out:
+            outt.append([Candidate(*c) for c in batch_out])
+        return outt
